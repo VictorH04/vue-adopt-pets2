9fd92f740f255d7b19ebbaca03297a989fe325cd
Victor_H <Victor.h7@live.com>
2021-01-30 17:17:35 +0100

    Added some routes and functionality

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
+
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/cats">Cats</router-link> |
+      <router-link to="/dogs">Dogs</router-link> 
+
     </div>
-    <router-view/>
+
+    <b-container>
+      <router-view />
+    </b-container>
+
   </div>
+
 </template>
 
 <style>
@@ -22,6 +30,7 @@
 }
 
 #nav a {
+  margin: 0 30px;
   font-weight: bold;
   color: #2c3e50;
 }
